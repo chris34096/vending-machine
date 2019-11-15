80ad0c9ba7023a413705d4891d6ae57a8b1afb90
Christofer Perez <christoferperez@gmail.com>
2019-11-15 14:05:52 +0100

    Phase 2: adding actions, containers, product-item, reducers folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
+import products from "./components/product-item/product-item.component";
+
+import data from "./data/data.json"; // fetch after
+
 import "./App.css";
 
-function App() {
-  return <div className="App">Vending machine v1.0</div>;
+class App extends Component {
+  render() {
+    return (
+      <div className="title">
+        <h1> Vending Machine v1.0 </h1>
+        <h2>number of product left : 58</h2>
+        <products />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/product-item/product-item.component.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class product extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Test 1</h1>
+      </div>
+    );
+  }
+}
+
+export default product;
