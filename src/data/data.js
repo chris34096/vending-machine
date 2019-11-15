a23aefb22ea057ccc46f95bf2bdc14510b9ac009
Christofer Perez <christoferperez@gmail.com>
2019-11-15 11:49:46 +0100

    Phase 1: Create react app and adding data.Js regarding product

---
+++ src/data/data.js
@@ -0,0 +1,52 @@
+const Snaking = [
+  {
+    id: 0,
+    name: "Twix",
+    price: 1
+  },
+  {
+    id: 1,
+    name: "Mars",
+    price: 1
+  },
+  {
+    id: 2,
+    name: "Bounty",
+    price: 2
+  },
+  {
+    id: 3,
+    name: "Snikers",
+    price: 2
+  },
+  {
+    id: 4,
+    name: "Evian",
+    price: 3
+  },
+  {
+    id: 5,
+    name: "Vittel",
+    price: 3
+  },
+  {
+    id: 6,
+    name: "Chips",
+    price: 4
+  },
+  {
+    id: 7,
+    name: "Oreo",
+    price: 4
+  },
+  {
+    id: 8,
+    name: "M&M",
+    price: 5
+  },
+  {
+    id: 9,
+    name: "Haribo",
+    price: 5
+  }
+];
